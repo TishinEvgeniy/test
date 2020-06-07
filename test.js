44c11fde815fa153b0bc92677bc84224bb1453a4
Evgeniy <jokertrade1@gmail.com>
2020-06-07 23:57:37 +0300

    первое изменение

---
+++ test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// alert('Hello world!');
+
+// let b = confirm('you age?');
+
+// console.log(b);
+
+// let b = prompt('you age 18?', 'да');
+
+// console.log(typeof(b));
+
+
+let incr = 10;
+let decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('3' == 3);
+
+let isChecked = true,
+    isClose = true;
+
+    console.log(isChecked || isClose);
